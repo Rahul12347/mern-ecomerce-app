dd4db7220dd8e9e1720f6aa1ce4d04a494ea766e
Rahul12347 <2019rahul.maurya@ves.ac.in>
2024-09-22 16:02:59 +0530

    all changes added

---
+++ client/src/store/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from "@reduxjs/toolkit";
+import adminOrderSlice from "./admin/order-slice";
+import adminProductsSlice from "./admin/products-slice";
+import authReducer from "./auth-slice";
+
+import commonFeatureSlice from "./common-slice";
+import shopAddressSlice from "./shop/address-slice";
+import shopCartSlice from "./shop/cart-slice";
+import shopOrderSlice from "./shop/order-slice";
+import shopProductsSlice from "./shop/products-slice";
+import shopReviewSlice from "./shop/review-slice";
+import shopSearchSlice from "./shop/search-slice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+
+    adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
+
+    shopProducts: shopProductsSlice,
+    shopCart: shopCartSlice,
+    shopAddress: shopAddressSlice,
+    shopOrder: shopOrderSlice,
+    shopSearch: shopSearchSlice,
+    shopReview: shopReviewSlice,
+
+    commonFeature: commonFeatureSlice,
+  },
+});
+
+export default store;
